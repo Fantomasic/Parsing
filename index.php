7d6736a822313e0b284b182acffec02a20069019
Fantomasic <44503761+Fantomasic@users.noreply.github.com>
2019-05-11 10:26:20 +0300

    Update3

--- index.php
+++ index.php
@@ -6,10 +6,14 @@ $app->initLayout('Centered');
 $html=file_get_contents('https://www.elkor.lv/rus/elektronika-1/speles/datorspeles/?game_age_restriction=5305');
 phpQuery::newDocument($html);
 $title = pq('#products-grid-table')->text();
-var_dump($title);
 $title = explode('Компьютерная игра',$title);
+foreach ($title as $game ) {
+  // code...
 
-
+$echo = str_replace ("Добавить в корзину","",$game);
+$echo = str_replace("Платеж в месяц","",$echo);
+echo $echo;
+}
 
 
 
