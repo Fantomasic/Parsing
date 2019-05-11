d1190a2d6e77814c85bdec99f7406b204ec8f5b6
Fantomasic <44503761+Fantomasic@users.noreply.github.com>
2019-05-11 10:48:28 +0300

    Update5

--- index.php
+++ index.php
@@ -7,26 +7,25 @@ $html=file_get_contents('https://www.elkor.lv/rus/elektronika-1/speles/datorspel
 phpQuery::newDocument($html);
 $title = pq('#products-grid-table')->text();
 $title = explode('Компьютерная игра',$title);
-foreach ($title as $game ) {
-  // code...
-
-$echo = str_replace ("Добавить в корзину","",$game);
-$echo = str_replace("Платеж в месяц","",$echo);
-$echo = str_replace((",",".",$echo);
-$echo = str_replace('€',"",$echo);
-echo $echo;
+foreach ($title as $game) {
+    $money = '';
+    $echo = str_replace("Добавить в корзину"," ",$game);
+    $echo = str_replace("Платёж в месяц","",$echo);
+    $echo = str_replace(",",".",$echo);
+    $echo = str_replace(' €',"",$echo);
+    if ($echo !== " ") {
+      for ($i = strlen($echo); $i >= 0; $i--){
+          if (is_numeric($echo[$i]) OR $echo[$i]=='.') {
+            $money = $echo[$i].$money;
+          }
+          if ((10<$money and $money<100) AND $echo[$i-1] !== '.') {
+            break;
+          }
+      }
+      $echo = str_replace($money,"",$echo);
+      $app->add(['Label',$echo,'big green']);
+      $app->add(['Label',$money.' €','small red']);
+      $app->add(['ui'=>"hidden divider"]);
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
 phpQuery::unloadDocuments($html);
